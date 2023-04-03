873e9d116fe59705e6e087c43a3aa671c36acae0
Gena <12708372+Gena1007@user.noreply.gitee.com>
2023-04-03 02:04:06 +0000

    !1 add 01_climbing_stairs/gena_climbing_stairs.java * add 01_climbing_stairs/gena_climb_stairs.java.

---
+++ 01_climbing_stairs/gena_climb_stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        int a = 0; 
+		int b = 0;
+		int c = 1;		
+		for(int i = 1; i <= n ; i++) {
+			a = b;
+			b = c;
+			c = a + b;
+		}
+		return c;	
+    }
+}
